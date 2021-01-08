4c9f39eb6c0d0d5a22ef74e68b752acc5eb6e233
euaaron <euaaron@outlook.com>
2021-01-08 14:38:39 -0300

    start: added basefiles

---
+++ src/shared/infra/http/routes/index.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+// routes.use('/');
+// routes.use('/profile');
+// routes.use('/sessions');
+// routes.use('/users');
+// routes.use('/events');
+// routes.use('/forums');
+// routes.use('/chat');
+
+export default routes;
---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import express from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+
+import routes from '@shared/infra/http/routes';
+import rateLimiter from './middlewares/rateLimiter';
+
+const core = express();
+const port = process.env.APP_PORT || 3333;
+
+core.use(cors());
+core.use(express.json());
+core.use(rateLimiter);
+core.use(routes);
+
+core.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`💻 Server started on port ${port}!`);
+});
